be17f5287446a7424ed5308719c561a8a2d71065
Ethan Loewen <ethanloewen@gmail.com>
2022-03-10 05:03:49 +0000

    Add protection from SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,7 +1,5 @@
 const { Pool } = require('pg');
 
-const args = process.argv.slice(2);
-
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -9,13 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const args = process.argv.slice(2);
+const cohortName = args[0];
+const limit = args[1] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,7 +1,5 @@
 const { Pool } = require('pg');
 
-const args = process.argv.slice(2);
-
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -9,15 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+const args = process.argv.slice(2);
+const cohortName = args[0];
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
